92500673bba2128be7aa0dd9a6d81b563cfb3529
Slicererer <mercedestoy1973@yahoo.com>
2025-04-12 12:43:40 -0500

    Souce Code Dont Ask

---
+++ main.js
@@ -0,0 +1,32 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webviewTag: true,
+    },
+    icon: path.join(__dirname, 'icon.ico'), // Add icon path
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
